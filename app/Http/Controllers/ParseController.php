3ed47040134462df71ceeee1eff35b031ca5edd4
Lester B. Hurtado <lester@applester.co>
2015-09-08 14:31:15 +0800

    Add Telehook class(4)

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -181,17 +181,17 @@ class ParseController extends Controller
                                     }
                                 }
                                 else {
-
+/*
                                     $data = array(
                                         'reply' => "You are now in recruiting mode. Please enter mobile number of your recruit:",
                                         'variables' => array(
                                             'state.id' => "recruiting",
                                         ),
                                     );
-
-                                    //$data = new Telehook("You, yes you, are now in recruiting mode. Please enter mobile number of your recruit:");
-                                    //$data->AddVariable('state.id|recuiting');
-                                    return response(view('webhook', $data), 200, ['Content-Type' => "application/json"]);
+*/
+                                    $data = new Telehook("You, yes you, are now in recruiting mode. Please enter mobile number of your recruit:");
+                                    $data->AddVariable('state.id|recruiting');
+                                    return response(view('webhook', $data->getData()), 200, ['Content-Type' => "application/json"]);
                                 }
                                 break;
                         }
