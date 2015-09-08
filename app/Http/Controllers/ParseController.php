14422e5b159dc912e48d0dc7563663ba411bbfec
Lester B. Hurtado <lester@applester.co>
2015-09-08 17:04:25 +0800

    Add Telehook class(14) just a reply; instantiate Telehook then destroy it

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -197,6 +197,7 @@ class ParseController extends Controller
 
                                     //var_dump($data);
                                     $tele = new Telehook("Testing 123");
+                                    unset($tele);
                                     //$tele->AddVariable('state.id|recruiting');
                                     //$tele->generateData($data2);
                                     //var_dump($data);
