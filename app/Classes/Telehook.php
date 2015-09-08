fd689f151778617ecc5c465504a808254e815c0f
Lester B. Hurtado <lester@applester.co>
2015-09-08 22:06:39 +0800

    Add Telehook class(28) just a reply; static array in Telehook

---
+++ app/Classes/Telehook.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Classes;
+
+/**
+ * Class Telehook
+ * @package App\Classes
+ */
+class Telehook
+{
+    public static $reply;
+
+    public static $forwards = array();
+
+    public static $variables = array();
+
+    public static $data = array();
+
+    public static function addReply($reply){
+        self::$reply = $reply;
+    }
+
+    public static function addForward($pipe_delimited_text)
+    {
+        $forward = explode('|', $pipe_delimited_text);
+        self::$forwards[$forward[0]] = $forward[1];
+    }
+
+    public static function addVariable($pipe_delimited_text)
+    {
+        $variable = explode('|', $pipe_delimited_text);
+        self::$variables[$variable[0]] = $variable[1];
+    }
+
+    public static function getData(){
+        $ar = array();
+        if (self::$reply)
+            $ar['reply'] = self::$reply;
+        if (self::$forwards)
+            $ar['forwards'] = self::$forwards;
+        if (self::$variables)
+            $ar['variables'] = self::$variables;
+
+        return $ar;
+    }
+}
