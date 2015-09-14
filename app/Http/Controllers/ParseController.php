3e759bcbebbcbc230ddbe747ac0207ef96648c15
Lester B. Hurtado <lester@applester.co>
2015-09-14 10:32:29 +0800

    Add debug for Telerivet webhook in Forge 12

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -184,6 +184,7 @@ class ParseController extends Controller
     function autoRecruit()
     {
         return Telehook::getInstance()->getDebugResponse('autorecruit');
+        
         if (Telehook::isAuthorized($this->request)) {
             Telehook::getInstance()
                 ->setReply('You are now in recruiting mode. Please enter mobile number of your recruit:')
