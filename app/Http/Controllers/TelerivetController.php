acde85110406613b453f9f7ddb8f9e0b4d768d1b
Lester B. Hurtado <lester@applester.co>
2015-09-14 08:12:14 +0800

    Add debug for Telerivet webhook in Forge 2

--- app/Http/Controllers/TelerivetController.php
+++ app/Http/Controllers/TelerivetController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Classes\Telehook;
 use App\Classes\MobileAddress;
+use Symfony\Component\Yaml\Exception\ParseException;
 
 define('TELEHOOK_NO_STATE', '');
 
@@ -40,38 +41,40 @@ class TelerivetController extends Controller
 
     public function webhook()
     {
-        return Telehook::getErrorResponse();
+        try {
+            switch (Telehook::$state) {
+                case TELEHOOK_NO_STATE:
+                    $command = Telehook::$keyword;
+                    $arguments = Telehook::$arguments;
+                    break;
+                default:
+                    $command = Telehook::$state;
+                    $pattern = array_get($this->commands, "$command.pattern");
+                    $arguments = preg_match($pattern, Telehook::$content, $matches)
+                        ? array_where($matches, function ($key) {
+                            return preg_match("/^[a-zA-Z]*$/", $key);
+                        })
+                        : array();
 
-        switch (Telehook::$state) {
-            case TELEHOOK_NO_STATE:
-                $command = Telehook::$keyword;
-                $arguments = Telehook::$arguments;
-                break;
-            default:
-                $command = Telehook::$state;
-                $pattern = array_get($this->commands, "$command.pattern");
-                $arguments = preg_match($pattern, Telehook::$content, $matches)
-                    ? array_where($matches, function ($key) {
-                        return preg_match("/^[a-zA-Z]*$/", $key);
-                    })
-                    : array();
-
-                $parameters = array_get($this->commands, "$command.parameters");
-                if (is_array($parameters)) {
-                    foreach (array_get($this->commands, "$command.parameters") as $parameter => $regex) {
-                        $found_record = array_where(Telehook::$inputs, function ($key) use ($regex) {
-                            return preg_match($regex, $key);
-                        });
-                        if ($found_record)
-                            $arguments[$parameter] = current($found_record);
+                    $parameters = array_get($this->commands, "$command.parameters");
+                    if (is_array($parameters)) {
+                        foreach (array_get($this->commands, "$command.parameters") as $parameter => $regex) {
+                            $found_record = array_where(Telehook::$inputs, function ($key) use ($regex) {
+                                return preg_match($regex, $key);
+                            });
+                            if ($found_record)
+                                $arguments[$parameter] = current($found_record);
+                        }
                     }
-                }
-                break;
+                    break;
+            }
+            //$url = route($command, $arguments, 307);
+            //return $url;
+            //return $arguments;
         }
-        //$url = route($command, $arguments, 307);
-        //return $url;
-        //return $arguments;
-
+        catch (ParseException $ex) {
+            return Telehook::getErrorResponse();
+        }
         return redirect()->route($command, $arguments, 307);
     }
 }
