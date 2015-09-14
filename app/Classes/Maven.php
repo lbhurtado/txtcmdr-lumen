3cb431d30db3757073961d7f890ad816415a8ebe
Lester B. Hurtado <lester@applester.co>
2015-09-14 22:41:34 +0800

    Modify state.id from verifying to verify (21).

--- app/Classes/Maven.php
+++ app/Classes/Maven.php
@@ -162,8 +162,8 @@ class Verify extends Maven
         //extract($this->getCommand()->getParameters(), EXTR_PREFIX_ALL, 'extracted');
         $somenumber = array_get($this->getCommand()->getParameters(), 'somenumber');
         $mobile = MobileAddress::getInstance($somenumber)->getServiceNumber();
-        //$text = implode(' ', array_keys($this->getCommand()->getParameters()));
-        $text = serialize(parent::$request->all());
+        $text = implode(' ', array_keys($this->getCommand()->getParameters()));
+        //$text = serialize(parent::$request->input['vars']);
 
         /*
 
