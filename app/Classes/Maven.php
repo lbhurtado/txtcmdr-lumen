7b8b520788ec4aa4df34d251316d1052c39aabc4
Lester B. Hurtado <lester@applester.co>
2015-09-14 21:59:44 +0800

    Modify state.id from verifying to verify (13).

--- app/Classes/Maven.php
+++ app/Classes/Maven.php
@@ -187,6 +187,6 @@ class Verify extends Maven
         }
         else
 */
-        return Telehook::getInstance()->getDebugResponse( $mobile );
+        return Telehook::getInstance()->getDebugResponse( $mobile ?: "not mobile" );
     }
 }
