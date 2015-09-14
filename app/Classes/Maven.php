05013cde61f05ca0ed73b50ca2cc34451845879e
Lester B. Hurtado <lester@applester.co>
2015-09-14 19:51:55 +0800

    Modify state.id from verifying to verify (6).

--- app/Classes/Maven.php
+++ app/Classes/Maven.php
@@ -156,8 +156,8 @@ class Verify extends Maven
 {
     public function getResponse()
     {
-        extract($this->getCommand()->getParameters(), EXTR_PREFIX_ALL, 'extracted');
-        $mobile = MobileAddress::getInstance($extracted_somenumber)->getServiceNumber();
+        //extract($this->getCommand()->getParameters(), EXTR_PREFIX_ALL, 'extracted');
+        //$mobile = MobileAddress::getInstance($extracted_somenumber)->getServiceNumber();
         /*
 
         if ($mobile) {
@@ -184,8 +184,6 @@ class Verify extends Maven
         }
         else
 */
-        return Telehook::getInstance()
-            ->setReply("$mobile id not a valid mobile number.")
-            ->getResponse();
+        return Telehook::getInstance()->getDebugResponse('Verify getResponse()');
     }
 }
